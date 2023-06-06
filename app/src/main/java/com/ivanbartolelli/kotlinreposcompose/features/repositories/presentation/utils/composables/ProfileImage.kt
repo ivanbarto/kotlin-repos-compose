7fe6d1310ed2478b9ee4cae050d00fbaeb3cf342
Ivan Bartolelli <ivanbartolelli@gmail.com>
2023-06-05 22:29:01 -0300

    error image placeholder added and minor build gradle refactor

--- app/src/main/java/com/ivanbartolelli/kotlinreposcompose/features/repositories/presentation/utils/composables/ProfileImage.kt
+++ app/src/main/java/com/ivanbartolelli/kotlinreposcompose/features/repositories/presentation/utils/composables/ProfileImage.kt
@@ -18,18 +18,20 @@ import com.ivanbartolelli.kotlinreposcompose.R
 import com.ivanbartolelli.kotlinreposcompose.core.presentation.theme.customColorsPalette
 
 @Composable
-fun ProfileImage(imageUrl : String?) {
+fun ProfileImage(imageUrl: String?) {
     Card(modifier = Modifier.clip(CircleShape), elevation = 3.dp) {
         AsyncImage(
             model = ImageRequest.Builder(LocalContext.current)
                 .data(imageUrl)
                 .crossfade(true)
                 .build(),
-            placeholder = painterResource(R.drawable.ic_user),
+
             contentDescription = stringResource(R.string.text_github_user),
             modifier = Modifier
                 .size(70.dp)
                 .background(MaterialTheme.customColorsPalette.profileImageBackground),
+            placeholder = painterResource(R.drawable.ic_user),
+            error = painterResource(R.drawable.ic_user),
         )
     }
 }
